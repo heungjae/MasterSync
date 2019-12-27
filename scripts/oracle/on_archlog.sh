587959aaf21eabcbcdcaf8bf1add36506187cff4
Michael Chew <michael.chew@actifio.com>
2019-12-27 11:52:48 +1100

    Update on_archlog.sh

--- scripts/oracle/on_archlog.sh
+++ scripts/oracle/on_archlog.sh
@@ -5,7 +5,13 @@ if [ "$1" = "" ] ; then
   exit 0
 fi
 
+# test -n "${ORACLE_SID:-}" || (echo "ORACLE_SID should be defined." && exit  1);
+
+export ORAENV_ASK=NO
 export ORACLE_SID=$1
+. oraenv
+export ORAENV_ASK=YES
+
 echo "Connecting to $ORACLE_SID db.."
 sqlplus /nolog <<EOF
 connect / as sysdba
