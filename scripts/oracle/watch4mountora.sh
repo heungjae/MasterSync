66d1fcb784cb6e129c9783630a1f5acfdb210a8e
Michael Chew <michael.chew@actifio.com>
2019-12-27 11:44:36 +1100

    Add files via upload

---
+++ scripts/oracle/watch4mountora.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# watch 'ps -ef | grep pmon | grep -v grep ; echo ""; echo ""; df -h'
+
+
+DBNAME=bigdb
+test -n "$1"  && DBNAME=$1
+
+echo $START_JOBID
+START_JOBID=`tac /var/act/log/UDSAgent.log | grep Job_ | sed -n 1p | grep -o "\w*Job\w*"`
+
+while : ; 
+do 
+clear ; 
+
+JOBID=`tac /var/act/log/UDSAgent.log | grep Job_ | sed -n 1p | grep -o "\w*Job\w*"`
+echo -e "  current date and time: `date "+%Y-%m-%d _ %H-%M-%S"`  JobID: $JOBID \n"
+
+printf %s%s%s $_{1..40}"-" " Oracle instance - process monitor " $_{1..40}"-" ; echo
+ps -ef | grep pmon | grep -v grep | grep -v "bash" ; echo -e " "
+
+printf %s%s%s $_{1..40}"-" " Oracle RMAN processes " $_{1..40}"-" ; echo
+ps -ef | grep -i rman | grep -v grep ; echo -e " "
+
+printf %s%s%s $_{1..40}"-" " Mounted filesystems " $_{1..40}"-" ; echo
+df -h | head -1 ; echo -e "......" ; df -h | grep -v tmpfs | tail -4 ; echo -e " "
+
+printf %s%s%s $_{1..40}"-" " tail Actifio connector log " $_{1..40}"-" ; echo
+tail -5 /var/act/log/UDSAgent.log ; echo -e " "
+
+printf %s%s%s $_{1..40}"-" " tail Actifio RMAN log for $DBNAME " $_{1..40}"-" ; echo
+tail -5 /var/act/log/${DBNAME}_rman.log ; echo -e " "
+
+printf %s%s%s $_{1..40}"-" " Actifio log directory " $_{1..40}"-" ; echo
+ls -lrt /var/act/log | tail -3 ; echo -e " "
+
+printf %s%s%s $_{1..40}"-" " Actifio mount jobs " $_{1..40}"-" ; echo
+echo -e "/act/mnt "
+ls -lrt /act/mnt | tail -3 ; echo -e " "
+echo -e "/act/tmpdata "
+ls -lrt /act/tmpdata | grep ^d | tail -2 ; echo -e " "
+
+# ./Job_0187424/bigdb__rman.conf
+
+sleep 8 ; 
+done
+
+# echo -e "\n" ; ps -ef | grep pmon | grep -v grep ; 
+# echo -e ''$_{1..60}'_'
+# echo -e " " ; df -h ;
+
