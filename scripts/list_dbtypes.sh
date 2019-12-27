2b619248b7bab2672c188c9f98ca4dbdcbc21c9a
Michael Chew <michael.chew@actifio.com>
2019-12-27 11:07:10 +1100

    Add files via upload

---
+++ scripts/list_dbtypes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#  Script needs 3 parameters: ActIP ActUser ActPass
+#
+
+readonly numparms=3
+
+usage ()
+{
+  echo -e "\n  Usage: $0 ActIP ActUser ActPass " >&2
+  echo -e "  Example: $0 10.65.5.35 admin TopSecret " >&2
+  echo -e "  Purpose: Lists all the applications using reportapps \n" >&2; exit 1;
+}
+
+[ $# -ne $numparms ] && usage
+
+export ActIP=$1
+ActUser=$2
+ActPass=$3
+
+session_id=`curl -sw "\n" -k -XPOST https://$ActIP/actifio/api/login?name=$ActUser\&password=$ActPass\&vendorkey=actifio | sed -e 's/[{}]/''/g' | awk -v RS=',"' -F: '/session/ {print $2}' | sed 's/\"//g'`
+# echo $session_id
+#curl -sS -w "\n" -k https://$ActIP/actifio/api/report/reportapps?sessionid=$session_id | jq -r '.[]'
+curl -sS -w "\n" -k https://$ActIP/actifio/api/report/reportapps?sessionid=$session_id
